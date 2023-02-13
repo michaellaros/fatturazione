7a4d809649a618b9d296faefb130d519c2a9f9a4
michaellaros <56259544+michaellaros@users.noreply.github.com>
2023-02-13 11:25:15 +0100

    speriamo non si spacchi nulla

--- src/app/components/filtro-ricevuta/filtro-ricevuta.component.ts
+++ src/app/components/filtro-ricevuta/filtro-ricevuta.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-filtro-ricevuta',
@@ -8,17 +7,4 @@ import { FormBuilder } from '@angular/forms';
 })
 export class FiltroRicevutaComponent {
    cassa!: string;
-  /*
-   public constructor(private fb: FormBuilder){}
-
-   ngInit(){
-    this.form = this.fb.group({
-      name: [null, [Validators.required, Validators.minLength(10)]],
-      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      dob: [null, [Validators.required]],
-      address: [null],
-      country: [null],
-      gender: [null]
-    });
-   }*/
 }
