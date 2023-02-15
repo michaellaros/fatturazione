9064fedbd0b964478d07b72b1a5699f92568d6bc
michaellaros <56259544+michaellaros@users.noreply.github.com>
2023-02-15 17:00:22 +0100

    try surname

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -4,6 +4,9 @@ import { Observable, throwError } from 'rxjs';
 import {catchError} from 'rxjs/operators';
 import { Ricevuta } from '../models/ricevuta';
 import { FiltroRicevuta } from '../models/filtroRicevuta';
+import { FiltroCliente } from '../models/filtroCliente';
+import { Cliente } from '../models/cliente';
+
 
 
 
@@ -24,17 +27,23 @@ export class HttpService {
     return this.http.get<{ricevute:Ricevuta[]}>(this.urlAPI+"Ricevuta/DB/")
                   .pipe(catchError((error:HttpErrorResponse) => throwError(() =>new Error(error.message || "Server error!"))));
   }
+
   GetRicevuta(fileName:string){
     console.log(this.urlAPI+"Ricevuta?fileName="+fileName);
     return this.http.post<Ricevuta>(this.urlAPI+"Ricevuta/GetRicevuta",{fileName})
                   .pipe(catchError((error:HttpErrorResponse) => throwError(() =>new Error(error.message || "Server error!"))));
   }
-  
+
+  RicercaCliente(filtriCliente:any){
+    return this.http.post<{clienti:Cliente[]}>(this.urlAPI+"Cliente/Cliente/",filtriCliente)
+                  .pipe(catchError((error:HttpErrorResponse) => throwError(() =>new Error( error.message || "Server error!"))));
+  }
+
   RicercaRicevuta(filtriRicevuta:any){
     return this.http.post<{ricevute:string[]}>(this.urlAPI+"Ricevuta/",filtriRicevuta)
                   .pipe(catchError((error:HttpErrorResponse) => throwError(() =>new Error( error.message || "Server error!"))));
   }
 
-  
+
 
 }
