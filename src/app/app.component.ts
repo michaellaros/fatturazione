e66e85c1a124ed6e70b7a944e7d830f27701b9dd
michaellaros <56259544+michaellaros@users.noreply.github.com>
2023-02-21 15:13:29 +0100

    estetica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,8 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     trigger('rotatedState', [
       state('default', style({ transform: 'rotate(0)' })),
       state('rotated', style({ transform: 'rotate(-180deg)' })),
-      transition('rotated => default', animate('400ms ease-out')),
-      transition('default => rotated', animate('400ms ease-in'))
+      transition('rotated => default', animate('150ms ease-out')),
+      transition('default => rotated', animate('150ms ease-in'))
     ])
     ]
 })
